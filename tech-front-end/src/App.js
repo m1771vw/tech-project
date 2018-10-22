3ef43c69e4afe7e35c07e4d6e7d3304eb4685b1b
James Park <jamestkd918@gmail.com>
2018-10-22 13:40:22 -0700

    #created skeleton of log in form (updates state value)

--- tech-front-end/src/App.js
+++ tech-front-end/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-
+import Login from './Components/Login'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div >
-        Hello world.
+
+        <Login />
       </div>
     );
   }

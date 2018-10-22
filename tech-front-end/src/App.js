3cd7f2e3e0859687b70f72968ebd7a7a38eafe14
James Park <jamestkd918@gmail.com>
2018-10-22 14:32:44 -0700

    #using lazyload npm for lazy loading.

--- tech-front-end/src/App.js
+++ tech-front-end/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Login from './Components/Login'
+import Loader from './Components/LazyLoad';
 import './App.css';
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
       <div >
 
         <Login />
+        <Loader />
       </div>
     );
   }
---
+++ tech-front-end/src/Components/LazyLoad.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import LazyLoad from 'react-lazy-load';
+class Loader extends Component {
+    state = {
+
+    }
+    render() {
+        return (
+
+            <div>
+                Scroll to load images.
+                <div className="filler" />
+                <LazyLoad height={762} offsetVertical={300}>
+                    <h1>JAMES</h1>
+                </LazyLoad>
+                
+                <div className="filler" />
+            </div>
+        );
+    }
+}
+
+export default Loader;

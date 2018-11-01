25cf848e573f7d1acf07d3fdd4878682442b1ad7
James Park <jamestkd918@gmail.com>
2018-11-01 02:55:40 -0700

    #styled navbar

--- tech-front-end/src/Components/Navbar.js
+++ tech-front-end/src/Components/Navbar.js
@@ -1,94 +1,85 @@
-import React from "react";
+import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 import { Link } from "react-router-dom";
 import { Menu, Icon } from "semantic-ui-react";
 import "../App.css";
 
+class Navbar extends Component {
+  state = {
+    activeItem: 'dashboard'
+  }
 
-const Navbar = props => {
-  return (
-    <Menu 
-    className='menu-bar'
-    inverted
-    color='blue'>
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
-      <Link to="/">
-        <Menu.Item
-          name="editorials"
-        // active
-        // onClick
-        >
-          Dashboard
-        </Menu.Item>{" "}
-      </Link>
+  render() {
 
-      {/* <Link to="/assignments">
-        <Menu.Item
-          name="editorials"
-        // active
-        // onClick
-        >
-          Assignments
+    return (
+      <Menu
+        className='menu-bar'
+        inverted
+        size='huge'
+        color='blue' >
+
+        <Link to="/">
+          <Menu.Item
+            name="dashboard"
+            active={this.state.activeItem === 'dashboard'}
+            onClick={this.handleItemClick}
+          >
+            Dashboard
         </Menu.Item>
-      </Link> */}
+        </Link>
 
-      <Link to="/employees">
-        <Menu.Item
-          name="editorials"
-        // active
-        // onClick
-        >
-          Employees
+        <Link to="/employees">
+          <Menu.Item
+            name="employees"
+            active={this.state.activeItem === 'employees'}
+            onClick={this.handleItemClick}
+          >
+            Employees
         </Menu.Item>
-      </Link>
+        </Link>
 
-      <Link to="/projects">
-        <Menu.Item
-          name="editorials"
-        // active
-        // onClick
-        >
-          Projects
+        <Link to="/projects">
+          <Menu.Item
+            name="projects"
+            active={this.state.activeItem === 'projects'}
+            onClick={this.handleItemClick}
+          >
+            Projects
         </Menu.Item>
-      </Link>
-      {localStorage.isAuthorized === 'true' ? 
-          <Link to='/logout'><Menu.Item
-            name='editorials'
-          // active
-          // onClick
-          >Logout</Menu.Item> </Link>
-          : <Link to='/login'><Menu.Item
-            name='editorials'
-          // active
-          // onClick
-          >Login</Menu.Item> </Link> 
-        
-      }
-     
+        </Link>
 
-      {/* <Link to='/logout'><Menu.Item
-        name='editorials'
-      // active
-      // onClick
-      >Logout</Menu.Item> </Link> */}
+        {localStorage.isAuthorized === 'true' ?
+          <Link to='/logout'>
+            <Menu.Item
+              name="logout"
+              position='right'
+              active={this.state.activeItem === 'logout'}
+              onClick={this.handleItemClick}
+            >Logout</Menu.Item> </Link>
+          : <Link to='/login'>
+          <Menu.Item 
+            name="login"
+            position='right'
+            active={this.state.activeItem === 'login'}
+            onClick={this.handleItemClick}>Login
+          </Menu.Item> 
+          </Link>
 
-      {/* <Menu.Item
-        name='       '>
-        {}
-      </Menu.Item> */}
-      <Menu.Item
-        className='menu-icon'
-        position='right'
-        name='editorials'>
-              <Icon circular color='white' name='user' /> {localStorage.currentUser}
-      </Menu.Item>
+        }
 
-    </Menu>
-  );
+        <Menu.Item
+          className='menu-icon'
+          position='right'
+          name='user'>
+          <Icon size='small' color='white' name='user' /> {localStorage.currentUser}
+        </Menu.Item>
+
+      </Menu >
+    )
+  }
 };
 
-// Navbar.propTypes = {
-
-// };
 
 export default Navbar;

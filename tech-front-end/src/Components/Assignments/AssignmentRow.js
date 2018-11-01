05a46dd958e1d0869046260e7c0bb29b19c69f0a
twmphoto <twmphoto@icloud.com>
2018-10-31 17:28:21 -0700

    Added assignment rows

--- tech-front-end/src/Components/Assignments/AssignmentRow.js
+++ tech-front-end/src/Components/Assignments/AssignmentRow.js
@@ -21,7 +21,19 @@ class AssignmentRow extends Component {
         await this.props.updateAssignment(model);
         this.closeAssignmentModal();
       }
-
+      determineStatus = (status_name) => {
+        switch (status_name) {
+            case 'Blocked':
+                return 'error';
+            case 'Waiting Merge':
+                return 'warning';
+            case 'Completed' || 'Release':
+                return 'positive';
+            case 'Not Started':
+            default:
+                return "";
+        }
+    };
     render() {
         let assignment_id = this.props.assignment.assignment_id
         let assignment_name = this.props.assignment.assignment_name
@@ -32,10 +44,44 @@ class AssignmentRow extends Component {
         let assignment_final_hours = this.props.assignment.assignment_final_hours;
         let project_id = this.props.assignment.project_id;  
         let project_name = this.props.assignment.project_name;
+        let status_id = this.props.assignment.status_id;
 
 
         return (
-            <div></div>
+            <Table.Row className={this.determineStatus(status_name)} key={assignment_id + assignment_name}>
+                                            <Table.Cell selectable><Link to={`/assignments/details/${assignment_id}`}>{assignment_name}</Link></Table.Cell>
+                                            <Table.Cell selectable><Link to={`/projects/details/${project_id}`}>{project_name}</Link></Table.Cell>
+                                            <Table.Cell>{status_name}</Table.Cell>
+                                            <Table.Cell>{assignment_start_date}</Table.Cell>
+                                            <Table.Cell>{assignment_end_date}</Table.Cell>
+                                            <Table.Cell>{assignment_est_hours}</Table.Cell>
+                                            <Table.Cell>{assignment_final_hours}</Table.Cell>
+
+                                            <Table.Cell>
+                                                <Modal
+                                                    onClose={this.closeAssignmentModal}
+                                                    open={this.state.assignmentModal}
+                                                    trigger={<Button color="black" onClick={() => { this.setState({ assignmentModal: true }) }}>Update</Button>} closeIcon>
+                                                    <Modal.Header>Update Assignment</Modal.Header>
+                                                    <Modal.Content>
+                                                        <Modal.Description>
+                                                            <AssignmentEdit onSubmit={this.onUpdateAssignmentModal}
+                                                                assignment_id={assignment_id}
+                                                                assignment_name={assignment_name}
+                                                                status_name={status_name}
+                                                                assignment_start_date={assignment_start_date}
+                                                                assignment_end_date={assignment_end_date}
+                                                                assignment_est_hours={assignment_est_hours}
+                                                                assignment_final_hours={assignment_final_hours}
+                                                                project_id={project_id}
+                                                                status_id={status_id}
+                                                            />
+                                                        </Modal.Description>
+                                                    </Modal.Content>
+                                                </Modal>
+                                                <Button color='red' onClick={() => this.props.deleteAssignment(assignment_id)}>Delete</Button>
+                                            </Table.Cell>
+                                        </Table.Row>
         );
     }
 }
